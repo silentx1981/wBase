76b71b4abd72267f5e45145087e4525cdf4be75f
Raffael Wyss <raffael.wyss@gmail.com>
2026-01-17 22:22:09 +0100

    Init-Laravel-Project

---
+++ Helper/Docker/WindowsPowershell.md
@@ -0,0 +1,8 @@
+# PowerShell commands
+
+## Start Docker Service
+* Open Powershell
+* Click ```wsl```
+* Change to the wBase directory ```cd /path/to/wBase```
+* Run the following command to start the Docker service:
+```docker compose up -d```
---
+++ laravel/config/config/cors.php
@@ -0,0 +1,25 @@
+<?php
+
+return [
+
+    'paths' => [
+        'api/*',
+        'sanctum/csrf-cookie',
+        'login',
+        'logout',
+    ],
+
+    'allowed_methods' => ['*'],
+
+    'allowed_origins' => [
+        'http://localhost:4200',
+    ],
+
+    'allowed_headers' => ['*'],
+
+    'exposed_headers' => [],
+
+    'max_age' => 0,
+
+    'supports_credentials' => true,
+];

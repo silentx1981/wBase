6c7b965ef0fb6db10fec1e8711d576903fc3f8fa
Raffael Wyss <raffael.wyss@gmail.com>
2026-01-17 22:37:29 +0100

    Init-Laravel-Project

---
+++ laravel/app/Http/Controllers/AuthController.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class AuthController extends Controller
+{
+    public function login(Request $request)
+    {
+        $credentials = $request->validate([
+            'email'    => ['required', 'email'],
+            'password' => ['required'],
+        ]);
+
+        if (! Auth::attempt($credentials)) {
+            return response()->json([
+                'message' => 'Ungültige Login-Daten',
+            ], 422);
+        }
+
+        $request->session()->regenerate();
+
+        return response()->json($request->user());
+    }
+
+    public function logout(Request $request)
+    {
+        Auth::guard('web')->logout();
+
+        $request->session()->invalidate();
+        $request->session()->regenerateToken();
+
+        return response()->json([
+            'message' => 'Logged out',
+        ]);
+    }
+}
